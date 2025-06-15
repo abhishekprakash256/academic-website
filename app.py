2cdc572b7682d468cfab2f68cdc3f79a8ab2f725
abhishekprakash256 <abhishekprakash47@gmail.com>
2025-06-15 19:35:15 -0400

    sever added

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ app = Flask(__name__)
 
 # Serve the main index.html on the route you want
 
-STATIC_DIR = '/Users/abhi/academic-website'
+## For local development, you can set the STATIC_DIR to the path where your static files are located. -- /Users/abhi/academic-website
+
+## For Production use the path where your static files are located. -- /home/ubuntu/academic-website
+
+STATIC_DIR = '/home/ubuntu/academic-website'
 
 @app.route('/')
 def serve_index():
@@ -21,6 +25,12 @@ def serve_index():
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory(STATIC_DIR, filename)
+
+
+# Gunicorn will use this
+app_wsgi = app
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5100)
